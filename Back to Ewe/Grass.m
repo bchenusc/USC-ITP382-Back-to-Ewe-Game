868e3756cbe56af47c40d5d7ce4832333dae2862
Jeffrey Chau <chaujeff@usc.edu>
2014-04-07 19:40:35 -0700

    Adding grass

---
+++ Back to Ewe/Grass.m
@@ -0,0 +1,22 @@
+//
+//  Grass.m
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/7/14.
+//  Copyright (c) 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import "Grass.h"
+
+@implementation Grass
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
