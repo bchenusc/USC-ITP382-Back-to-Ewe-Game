b1cf3c7c5acd65f5d00a1fe53e4ad396135d87a3
Jeffrey Chau <chaujeff@usc.edu>
2014-04-13 18:43:34 -0700

    Added projectile file to project

---
+++ Back to Ewe/Classes/Projectile.h
@@ -0,0 +1,16 @@
+//
+//  Projectile.h
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/13/14.
+//  Copyright 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Projectile : CCNode {
+    
+}
+
+@end
