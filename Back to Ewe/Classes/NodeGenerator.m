5c27635bfd6187e40945bd764fd28395046c73da
Brian Chen <brianych@usc.edu>
2014-04-20 15:08:08 -0700

    fixed warning.s

--- Back to Ewe/Classes/NodeGenerator.m
+++ Back to Ewe/Classes/NodeGenerator.m
@@ -27,18 +27,10 @@
 
 //Generate a node pattern to appear on the screen.
 -(CGPoint)generatePattern: (GameplayScene*) gameplayScene{
-    //1. If it is a boss level, then spawn a boss level node pattern.
-    if (waveNum > 9 && waveNum % 10 == 0){
-        //Boss level!
-        waveNum ++;
-        int bossLevel = waveNum / 10;
-    }else{
     //2. If wave is not a boss level, then spawn a regular node pattern.
         //Random number generator --> currently 1-3
         waveNum ++;
-        return [nodePattern ChooseRandomPattern:waveNum :gameplayScene];
-    }
-    return ccp(0, 0);
+    return [nodePattern ChooseRandomPattern:waveNum :gameplayScene];
 }
 
 @end
