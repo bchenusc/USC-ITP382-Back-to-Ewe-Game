eafb34b39da940233e09b84c07a3ecc572132224
Jeffrey Chau <chaujeff@usc.edu>
2014-04-27 13:28:29 -0700

    UI Wool Meter added to project

---
+++ Back to Ewe/Classes/UIWoolMeter.h
@@ -0,0 +1,16 @@
+//
+//  UIWoolMeter.h
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/27/14.
+//  Copyright 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface UIWoolMeter : CCNode {
+    
+}
+
+@end
---
+++ Back to Ewe/Classes/UIWoolMeter.m
@@ -0,0 +1,14 @@
+//
+//  UIWoolMeter.m
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/27/14.
+//  Copyright 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import "UIWoolMeter.h"
+
+
+@implementation UIWoolMeter
+
+@end
