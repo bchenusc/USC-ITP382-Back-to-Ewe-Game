76df16713944b4a62d5ffb96aa8f9bae869c34ec
Jeffrey Chau <chaujeff@usc.edu>
2014-04-28 02:26:14 -0700

    Added health meter class to project

---
+++ Back to Ewe/Classes/UIHealthMeter.h
@@ -0,0 +1,16 @@
+//
+//  UIHealthMeter.h
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/27/14.
+//  Copyright 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface UIHealthMeter : CCNode {
+    
+}
+
+@end
---
+++ Back to Ewe/Classes/UIHealthMeter.m
@@ -0,0 +1,14 @@
+//
+//  UIHealthMeter.m
+//  Back to Ewe
+//
+//  Created by Jeffrey on 4/27/14.
+//  Copyright 2014 Silly Landmine Studios. All rights reserved.
+//
+
+#import "UIHealthMeter.h"
+
+
+@implementation UIHealthMeter
+
+@end
